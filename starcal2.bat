309728343ad56ab7936dc652f21a2fb91b2ac102
Saeed Rasooli <saeed.gnu@gmail.com>
2011-04-11 23:03:59 +0430

    qt config importer, pyw to py

--- starcal2.bat
+++ starcal2.bat
@@ -2,11 +2,11 @@
 for %%F in (%0) do set rootDir=%%~dpF
 
 if not exist %APPDATA%\starcal2 (
-  set IMP=scal2\ui_qt\config_importer.pyw
+  set IMP=scal2\ui_qt\config_importer.py
   if exist %rootDir%\%IMP% (
     %rootDir%\run.bat %IMP%
   )
 )
 
-%rootDir%\run.bat scal2\ui_qt\starcal_qt.pyw
+%rootDir%\run.bat scal2\ui_qt\starcal_qt.py
 
