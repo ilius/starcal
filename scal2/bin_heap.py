54ce1675eda20127747f8dc4dacd7a45b8f630ab
Saeed Rasooli <saeed.gnu@gmail.com>
2013-03-11 16:42:56 +0330

    implement MaxHeap.getMin()

--- scal2/bin_heap.py
+++ scal2/bin_heap.py
@@ -1,4 +1,4 @@
-import math
+from math import log
 from scal2.utils import s_join
 
 from heapq import heappush
@@ -21,6 +21,7 @@ class MaxHeap(list):
             self.moreThanStep(key, 2*index+1) + \
             self.moreThanStep(key, 2*index+2)
     __str__ = lambda self: ' '.join(['%s:%s'%(-k, v) for k, v in self])
+    keyCmp = lambda x1, x2: cmp(x1[0], x2[0])
     def delete(self, key, value):
         try:
             list.remove(self, (-key, value))
@@ -32,6 +33,39 @@ class MaxHeap(list):
             raise ValueError('heap empty')
         k, v = self[0]
         return -k, v
+    def getMin(self):
+        ## at least 2 times faster than max(self)
+        if not self:
+            raise ValueError('heap empty')
+        k, v = max(
+            self[-2**int(
+                log(len(self), 2)
+            ):]
+        )
+        return -k, v
+
+
+
+def getMinTest(N):
+    from random import randint
+    from time import time
+    h = MaxHeap()
+    for i in range(N):
+        x = randint(1, 10*N)
+        h.add(x, 0)
+    t0 = time()
+    k1 = -max(h)[0]
+    t1 = time()
+    k2 = h.getMin()[0]
+    t2 = time()
+    assert k1 == k2
+    #print 'time getMin(h)/min(h) = %.5f'%((t2-t1)/(t1-t0))
+    #print 'min key = %s'%k1
+
+#if __name__=='__main__':
+#    for i in range(10000):
+#        getMinTest(100)
+
 
 
 
