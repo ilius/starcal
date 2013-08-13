502fe8643a69a021232d31fb18876c26440d2ac3
Saeed Rasooli <saeed.gnu@gmail.com>
2013-08-13 20:44:54 +0430

    add zoneinfo-tree.json based on file list of /usr/share/zoneinfo

---
+++ scripts/load-zoneinfo-tree.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+import os
+from os import listdir
+from os.path import join, isfile, isdir
+from pprint import pprint, pformat
+from collections import OrderedDict
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+dataToPrettyJson = lambda data: json.dumps(data, sort_keys=False, indent=4)
+dataToCompactJson = lambda data: json.dumps(data, sort_keys=False, separators=(',', ':'))
+jsonToData = json.loads
+
+endObj = '' ## '' or None
+
+infoDir = ['usr', 'share', 'zoneinfo']
+
+groups = [
+    'Africa',
+    'America',
+    'Antarctica',
+    'Arctic',
+    'Asia',
+    'Atlantic',
+    'Australia',
+    'Brazil',
+    'Canada',
+    'Chile',
+    'Europe',
+    'Indian',
+    'Mexico',
+    'Mideast',
+    'Pacific',
+]
+
+zoneTree = OrderedDict()
+zoneNamesLevel = [[] for i in range(4)]
+
+def addZoneNode(parentDict, zone):
+    path = '/' + join(*tuple(infoDir + zone))
+    name = zone[-1]
+    zoneNamesLevel[len(zone)].append(name)
+    if isfile(path):
+        parentDict[name] = endObj
+    elif isdir(path):
+        parentDict[name] = OrderedDict()
+        for chName in sorted(listdir(path)):
+            addZoneNode(parentDict[name], zone + [chName])
+    else:
+        print 'invalid path =', path
+
+
+for group in sorted(groups):
+    addZoneNode(zoneTree, [group])
+
+zoneNamesList = []
+for levelNames in zoneNamesLevel:
+    zoneNamesList += sorted(levelNames)
+
+open('zoneinfo-tree.json', 'w').write(dataToPrettyJson(zoneTree).replace(' \n', '\n'))
+#open('zoneinfo-names.json', 'w').write(dataToPrettyJson(zoneNamesList))
+
+
