b10e7cf4814d7e96d0c5c9c5108faa5ba269c073
Saeed Rasooli <saeed.gnu@gmail.com>
2013-10-12 19:13:25 +0330

    use relative import in lib/tzlocal/__init__.py

--- scal2/lib/tzlocal/__init__.py
+++ scal2/lib/tzlocal/__init__.py
@@ -1,6 +1,6 @@
 import sys
 if sys.platform == 'win32':
-    from tzlocal.win32 import get_localzone, reload_localzone
+    from .win32 import get_localzone, reload_localzone
 else:
-    from tzlocal.unix import get_localzone, reload_localzone
+    from .unix import get_localzone, reload_localzone
     
