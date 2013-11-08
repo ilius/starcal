07157eab97b11dd1900bdce96215ee76b42a7bb0
Saeed Rasooli <saeed.gnu@gmail.com>
2013-11-08 09:04:35 +0330

    bug fix in dayTimeRange event rule, in case end_time <= start_time

--- scal2/event_lib.py
+++ scal2/event_lib.py
@@ -728,6 +728,8 @@ class DayTimeRangeEventRule(EventRule):
     def calcOccurrence(self, startJd, endJd, event):
         daySecStart = getSecondsFromHms(*self.dayTimeStart)
         daySecEnd = getSecondsFromHms(*self.dayTimeEnd)
+        if daySecEnd <= daySecStart:
+            daySecEnd = daySecStart + epsTm
         tmList = []
         for jd in range(startJd, endJd):
             epoch = getEpochFromJd(jd)
