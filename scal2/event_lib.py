f5ec9ab40256352760b46824d102ba71db327bd5
Saeed Rasooli <saeed.gnu@gmail.com>
2014-03-09 23:06:06 +0330

    don't exit if failed to create account object

--- scal2/event_lib.py
+++ scal2/event_lib.py
@@ -3797,7 +3797,11 @@ class EventAccountsHolder(JsonObjectsHolder):
                     cls = self.loadClass(name)
                     if cls is None:
                         continue
-                    obj = cls(_id)
+                    try:
+                        obj = cls(_id)
+                    except:
+                        myRaise()
+                        continue
                     #data['id'] = _id ## FIXME
                     obj.setData(data)
                 else:
