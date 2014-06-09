0c5603c786428b36711b95ee4a0b22f46159f6ba
Saeed Rasooli <saeed.gnu@gmail.com>
2014-06-09 21:08:10 +0430

    version 2.3.4

--- scal2/core.py
+++ scal2/core.py
@@ -48,7 +48,7 @@ except NameError:
     __file__ = join(dirname(inspect.getfile(scal2)), 'core.py')
 
 
-VERSION = '2.3.3'
+VERSION = '2.3.4'
 BRANCH = join(rootDir, 'branch')
 APP_NAME = 'starcal2'
 APP_DESC = 'StarCalendar'
