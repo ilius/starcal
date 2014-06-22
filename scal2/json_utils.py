63869929f1ad1dab21fd4caa5619825131858a2f
Saeed Rasooli <saeed.gnu@gmail.com>
2014-06-22 21:00:46 +0430

    cleaning json_utils.py

--- scal2/json_utils.py
+++ scal2/json_utils.py
@@ -5,8 +5,21 @@ except ImportError:
 
 from scal2.lib import OrderedDict
 
-dataToPrettyJson = lambda data: json.dumps(data, sort_keys=False, indent=2)
-dataToCompactJson = lambda data: json.dumps(data, sort_keys=False, separators=(',', ':'))
+dataToPrettyJson = lambda data: json.dumps(
+    data,
+    sort_keys=False,
+    indent=2,
+)
+
+dataToCompactJson = lambda data: json.dumps(
+    data,
+    sort_keys=False,
+    separators=(',', ':'),
+)
+
 jsonToData = json.loads
-jsonToOrderedData = lambda text: json.JSONDecoder(object_pairs_hook=OrderedDict).decode(text)
+
+jsonToOrderedData = lambda text: json.JSONDecoder(
+    object_pairs_hook=OrderedDict,
+).decode(text)
 
