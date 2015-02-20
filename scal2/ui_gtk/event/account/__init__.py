57359a89f71e451d491ee6ad0c4adafa1a2ad159
Saeed Rasooli <saeed.gnu@gmail.com>
2015-02-20 10:19:47 +0330

    fix missing import again in ui_gtk/event/account/__init__.py

--- scal2/ui_gtk/event/account/__init__.py
+++ scal2/ui_gtk/event/account/__init__.py
@@ -2,10 +2,11 @@
 
 from scal2 import core
 from scal2.locale_man import tr as _
+
 from scal2 import ui
 
 from scal2.ui_gtk import *
-from scal2.ui_gtk.utils import IdComboBox
+from scal2.ui_gtk.utils import IdComboBox, showError
 
 class BaseWidgetClass(gtk.VBox):
     def __init__(self, account):
