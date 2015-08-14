524c67870a86386ec38ee8fc19ddc91fbce44801
Saeed Rasooli <saeed.gnu@gmail.com>
2015-08-14 15:52:45 +0430

    show_object.py: use pretty json instead of pprint

--- scal3/show_object.py
+++ scal3/show_object.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 import sys
+import json
 from pprint import pprint
 from scal3.s_object import loadBsonObject
 
+dataToPrettyJson = lambda data: json.dumps(
+    data,
+    sort_keys=True,
+    indent=4,
+    ensure_ascii=False,
+)
+
 if __name__=='__main__':
     for arg in sys.argv[1:]:
-        pprint(loadBsonObject(arg))
+        data = loadBsonObject(arg)
+        #pprint(data, indent=4, width=80)
+        print(dataToPrettyJson(data))
         print('-------------------')
 
 
