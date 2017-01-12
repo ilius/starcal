187bd464689df1b8a086cccc150710eac61e805c
Saeed Rasooli <saeed.gnu@gmail.com>
2017-01-12 10:13:37 +0330

    add scripts/show_event

---
+++ scal3/show_event.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import json
+
+from scal3.s_object import loadBsonObject
+from scal3.json_utils import dataToPrettyJson
+from scal3 import event_lib
+
+
+if __name__ == '__main__':
+	eventId = int(sys.argv[1])
+	with open(event_lib.Event.getFile(eventId)) as eventFile:
+		eventJsonData = json.load(eventFile)
+
+	lastHash = eventJsonData['history'][0][1]
+	data = loadBsonObject(lastHash)
+	print(dataToPrettyJson(data))
