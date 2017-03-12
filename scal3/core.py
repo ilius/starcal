ea6007cb8535e1e14d7a11437c3c3aaa5b642ae5
Saeed Rasooli <saeed.gnu@gmail.com>
2017-03-12 11:23:00 +0800

    version 3.0.6

--- scal3/core.py
+++ scal3/core.py
@@ -49,7 +49,7 @@ except NameError:
     __file__ = join(os.path.dirname(inspect.getfile(scal3)), 'core.py')
 
 
-VERSION = '3.0.5'
+VERSION = '3.0.6'
 BRANCH = join(rootDir, 'branch')
 APP_DESC = 'StarCalendar'
 COMMAND = APP_NAME
