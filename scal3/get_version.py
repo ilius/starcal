0985b8dbc1c01b3f26f9d4a59e1bfc0f70a27aa2
Saeed Rasooli <saeed.gnu@gmail.com>
2017-03-12 21:07:03 +0800

    use a more accurate version string in install scripts if .git directory is present for tagged commits use the same tag, for non-tagged commits use "3.0.5-23-ge2e88f1" format

--- scal3/get_version.py
+++ scal3/get_version.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python3
 import os
+import sys
+import subprocess
 
-VERSION = ''
+srcDir = os.path.dirname(__file__)
+rootDir = os.path.dirname(srcDir)
 
-fp = open('%s/core.py'%os.path.dirname(__file__))
+gitDir = os.path.join(rootDir, ".git")
+if os.path.isdir(gitDir):
+    try:
+        outputB, error = subprocess.Popen(
+            [
+                "git",
+                "--git-dir", gitDir,
+                "describe",
+                "--tags",
+                "--always",
+            ],
+            stdout=subprocess.PIPE,
+        ).communicate()
+        # if error == None:
+        print(outputB.decode("utf-8").strip())
+        sys.exit(0)
+    except Exception as e:
+        sys.stderr.write(str(e) + "\n")
+
+
+VERSION = ""
+fp = open("%s/core.py" % srcDir)
 while True:
     line = fp.readline()
-    if line.startswith('VERSION'):
+    if line.startswith("VERSION"):
         exec(line)
         break
 print(VERSION)
