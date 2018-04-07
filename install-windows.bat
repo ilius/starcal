04d5a869937b2101f8d7265c5daf4311d2173074
Saeed Rasooli <saeed.gnu@gmail.com>
2018-04-07 14:13:43 +0800

    add install-windows.bat

---
+++ install-windows.bat
@@ -0,0 +1,15 @@
+REM Install Python 3.4 (because pygi-aio does not work with Python 3.6)
+REM Add to environment variable: Python34/ and Python34/Scripts
+REM Download latest pygi-aio from: https://sourceforge.net/projects/pygobjectwin32/files/
+REM Run pygi-aio, check these packages:
+REM - Base packages
+REM - GDK-Pixbuf
+REM - GTK+
+
+
+REM Install other dependencies:
+pip3 install python-dateutil
+pip3 install pymongo
+pip3 install psutil
+
+
