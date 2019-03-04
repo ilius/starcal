7be102a3127f651d6d7141cdbbe602e33b2d7734
Saeed Rasooli <saeed.gnu@gmail.com>
2019-03-04 03:46:49 +0330

    fix the font family and size in Season Progress Bar and Year Progress Bar

--- scal3/ui_gtk/pbar.py
+++ scal3/ui_gtk/pbar.py
@@ -1,3 +1,5 @@
+from scal3 import ui
+from scal3.ui_gtk.font_utils import pfontEncode
 from scal3.ui_gtk import *
 
 class MyProgressBar(gtk.ProgressBar):
@@ -9,3 +11,15 @@ class MyProgressBar(gtk.ProgressBar):
 		color = self.get_style_context().get_color(gtk.StateFlags.NORMAL)
 		color.alpha = 1.0
 		self.override_color(gtk.StateFlags.NORMAL, color)
+
+		# fix the font family and size
+		self.update_font()
+
+		# override_color and override_font are deprecated since version 3.16
+		# override_color: doc says: Use a custom style provider and style classes instead
+		# override_font: This function is not useful in the context of CSS-based rendering.
+		# If you wish to change the font a widget uses to render its text you should use a custom CSS style,
+		# through an application-specific Gtk.StyleProvider and a CSS style class.
+
+	def update_font(self):
+		self.override_font(pfontEncode(ui.getFont()))
