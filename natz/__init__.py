93ee990bf64930de6dbeea2477d8ec717fa03e49
Saeed Rasooli <saeed.gnu@gmail.com>
2019-04-22 16:01:14 +0430

    fix timezone = "etc/localtime", that might have been stored in events or shown in GUI and fix space indentation in natz/__init__.py

--- natz/__init__.py
+++ natz/__init__.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 import os
+from os.path import split, join
 import datetime
 import dateutil.tz
 
 class tzfile(datetime.tzinfo):
 	def __init__(self, tz):
-            self._tz = tz
-            if os.sep == "\\":
-                self._name = tz.tzname(datetime.datetime.now())
-            else:
-                parts = tz._filename.split("/")
-                self._name = "/".join(parts[-2:])
-
+		self._tz = tz
+		if os.sep == "\\":
+			self._name = tz.tzname(datetime.datetime.now())
+		else:
+			parts = tz._filename.split("/")
+			self._name = "/".join(parts[-2:])
 
 	def __str__(self):
 		# This is the only function that we needed to override on dateutil.tz.tzfile
@@ -27,9 +27,24 @@ class tzfile(datetime.tzinfo):
 		return self._tz.tzname(dt)
 
 
+def readEtcLocaltime():
+	fpath = os.readlink("/etc/localtime")
+	parts = fpath.split("/")
+	try:
+		index = parts.index("zoneinfo")
+	except:
+		print("Unexpected timezone file:", fpath)
+		return
+	tzname = "/".join(parts[index+1:])
+	return dateutil.tz.gettz(tzname)
+
 def gettz(*args, **kwargs):
 	tz = dateutil.tz.gettz(*args, **kwargs)
 	if tz is None:
 		return None
+	if tz._filename == "/etc/localtime":
+		tz = readEtcLocaltime()
+		if tz is None:
+			return None
 	return tzfile(tz)
 
