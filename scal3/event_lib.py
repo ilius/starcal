a542969294976309b5e056357a85c1f819e134a9
Saeed Rasooli <saeed.gnu@gmail.com>
2019-04-22 13:08:01 +0430

    always scan list of event/group files to find the last eventId/groupId on startup

--- scal3/event_lib.py
+++ scal3/event_lib.py
@@ -172,8 +172,35 @@ class LastIdsWrapper(JsonEventObj):
 		self.group = 0
 		self.account = 0
 
+	def __str__(self):
+		return "LastIds(event=%s, group=%s, account=%s)" % (self.event, self.group, self.account)
+
+	def scanDir(self, dpath):
+		lastId = 0
+		for fname in os.listdir(join(confDir, dpath)):
+			idStr, ext = splitext(fname)
+			if ext != ".json":
+				continue
+			try:
+				_id = int(idStr)
+			except:
+				print("invalid file name:", dpath)
+				continue
+			if _id > lastId:
+				lastId = _id
+		return lastId
+
+	def scan(self):
+		t0 = now()
+		self.event = self.scanDir("event/events")
+		self.group = self.scanDir("event/groups")
+		self.account = self.scanDir("event/accounts")
+		self.save()
+		print("Scanning last_ids took %.3f seconds, %s" % (now() - t0, self))
+
 
 lastIds = LastIdsWrapper.load()
+lastIds.scan()
 
 ###########################################################################
 
