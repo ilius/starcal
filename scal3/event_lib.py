ilius/starcal

commit 295190a4186b9dbe7d04c66d9c22b8c00556d565

add Event History feature to Event Manager Dialog

scal3/event_lib.py
@@ -1122,16 +1122,28 @@ class DurationEventRule(EventRule):
 	sgroup = 1
 	units = (1, 60, 3600, dayLen, 7 * dayLen)
 
+	def __str__(self):
+		return _("%s " + self.getUnitDesc()) % _(self.value)
+
+	def getUnitDesc(self):
+		return {
+			1:              "seconds",
+			60:             "minutes",
+			3600:           "hours",
+			3600 * 24:      "days",
+			3600 * 24 * 7:  "weeks",
+		}[self.unit]
+
 	def getServerString(self):
 		return "%d %s" % (self.value, self.getUnitSymbol())
 
 	def getUnitSymbol(self):
 		return {
-			1:				"s",
-			60:				"m",
-			3600:			"h",
-			3600 * 24:		"d",
-			3600 * 24 * 7:	"w",
+			1:              "s",
+			60:             "m",
+			3600:           "h",
+			3600 * 24:      "d",
+			3600 * 24 * 7:  "w",
 		}[self.unit]
 
 	def __init__(self, parent):
@@ -1898,10 +1910,25 @@ class Event(BsonHistEventObj, RuleContainer):
 			self.icon = group.icon
 
 	def getInfo(self):
+		mode = self.mode
+		calType, ok = calTypes[mode]
+		if not ok:
+			raise RuntimeError("cal type %r not found" % mode)
 		lines = []
+		lines.append(_("Type") + ": " + self.desc)
+		lines.append(_("Calendar Type") + ": " + calType.desc)
+		lines.append(_("Summary") + ": " + self.getSummary())
+		lines.append(_("Description") + ": " + self.description)
+		# "notifiers",
+		# "notifyBefore",
+		# "remoteIds",
+		# "lastMergeSha1",
+		# "modified",
+
 		rulesDict = self.rulesOd.copy()
 		for rule in rulesDict.values():
 			lines.append(rule.getInfo())
+
 		return "\n".join(lines)
 
 	#def addRequirements(self):
@@ -3821,11 +3848,14 @@ class EventGroup(EventContainer):
 	# event objects should be accessed from outside
 	# only via one of the following 3 methods
 
+	def removeFromCache(self, eid):
+		if eid in self.eventCache:
+			return self.eventCache[eid]
+
 	def getEvent(self, eid):
 		if eid not in self.idList:
 			raise ValueError("%s does not contain %s" % (self, eid))
-		if eid in self.eventCache:
-			return self.eventCache[eid]
+		self.removeFromCache(eid)
 		event = EventContainer.getEvent(self, eid)
 		event.parent = self
 		event.rulesHash = event.getRulesHash()

locale.d/fa.po
@@ -1123,6 +1123,118 @@ msgid "Edit Trash"
 msgstr ""
 "ویرایش زباله‌دان"
 
+msgid "History"
+msgstr ""
+"تاریخچه"
+
+msgid "Event History"
+msgstr ""
+"تاریخچهٔ رویداد"
+
+msgid "Group History"
+msgstr ""
+"تاریخچهٔ گروه"
+
+msgid "Change Summary"
+msgstr ""
+"خلاصهٔ تغییرات"
+
+msgid "(Added Event)"
+msgstr ""
+"(رویداد اضافه شد)"
+
+msgid "%s parameters"
+msgstr ""
+"%s پارامتر"
+
+msgid "Revert"
+msgstr ""
+"خنثی‌سازی"
+
+msgid "Revert this vhange"
+msgstr ""
+"خنثی‌سازی این تغییر"
+
+msgid "Select revision after this change"
+msgstr ""
+"انتخاب نسخهٔ بعد از این تغییر"
+
+msgid "Select revision before this change"
+msgstr ""
+"انتخاب نسخهٔ قبل از این تغییر"
+
+msgid "View type"
+msgstr ""
+"نوع نمایش"
+
+msgid "Change Table"
+msgstr ""
+"جدول تغییرات"
+
+msgid "Full Table"
+msgstr ""
+"جدول کامل"
+
+msgid "After change (Text)"
+msgstr ""
+"بعد از تغییر (متنی)"
+
+msgid "After change (JSON)"
+msgstr ""
+"بعد از تغییر (JSON)"
+
+msgid "After change (Plain JSON)"
+msgstr ""
+"بعد از تغییر (JSON هموار)"
+
+msgid "Before change (Text)"
+msgstr ""
+"قبل از تغییر (متنی)"
+
+msgid "Before change (JSON)"
+msgstr ""
+"قبل از تغییر (JSON)"
+
+msgid "Before change (Plain JSON)"
+msgstr ""
+"قبل از تغییر (JSON هموار)"
+
+msgid "Change (JSON Diff)"
+msgstr ""
+"تغییر (JSON Diff)"
+
+msgid "Key"
+msgstr ""
+"کلید"
+
+msgid "Old Value"
+msgstr ""
+"مقدار قبلی"
+
+msgid "New Value"
+msgstr ""
+"مقدار جدید"
+
+msgid "%s seconds"
+msgstr ""
+"%s ثانیه"
+
+msgid "%s minutes"
+msgstr ""
+"%s دقیقه"
+
+msgid "%s hours"
+msgstr ""
+"%s ساعت"
+
+msgid "%s days"
+msgstr ""
+"%s روز"
+
+msgid "%s weeks"
+msgstr ""
+"%s هفته"
+
 msgid "Move to %s"
 msgstr ""
 "جابجایی به %s"

pixmaps/history.svg
@@ -1,0 +1,140 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!-- Created with Inkscape (http://www.inkscape.org/) -->
+
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   width="320"
+   height="320"
+   viewBox="0 0 320 319.99999"
+   id="svg2"
+   version="1.1"
+   inkscape:version="0.91 r13725"
+   sodipodi:docname="history.svg"
+   inkscape:export-filename="/starcal/pixmaps/history-24.png"
+   inkscape:export-xdpi="6.75"
+   inkscape:export-ydpi="6.75">
+  <defs
+     id="defs4" />
+  <sodipodi:namedview
+     id="base"
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1.0"
+     inkscape:pageopacity="0.0"
+     inkscape:pageshadow="2"
+     inkscape:zoom="1.4142136"
+     inkscape:cx="101.65009"
+     inkscape:cy="201.5966"
+     inkscape:document-units="px"
+     inkscape:current-layer="layer1"
+     showgrid="false"
+     units="px"
+     inkscape:window-width="1908"
+     inkscape:window-height="965"
+     inkscape:window-x="0"
+     inkscape:window-y="35"
+     inkscape:window-maximized="1"
+     showguides="true"
+     inkscape:guide-bbox="true" />
+  <metadata
+     id="metadata7">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+        <dc:title />
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1"
+     transform="translate(0,-732.3622)">
+    <rect
+       style="opacity:1;fill:#ffffff;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:18.74200058;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1"
+       id="rect3345"
+       width="320"
+       height="320"
+       x="0"
+       y="732.36218"
+       rx="41.73"
+       inkscape:export-xdpi="9"
+       inkscape:export-ydpi="9" />
+    <rect
+       style="opacity:1;fill:none;fill-opacity:1;fill-rule:nonzero;stroke:#000000;stroke-width:16.19084167;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1"
+       id="rect4173"
+       width="133.81145"
+       height="226.51796"
+       x="110.56075"
+       y="810.94617" />
+    <path
+       sodipodi:nodetypes="cc"
+       inkscape:connector-curvature="0"
+       id="path4186"
+       d="m 145.93884,854.62321 60.65932,0"
+       style="fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:8.85148525;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+    <g
+       id="g4210"
+       transform="matrix(1.0537483,0,0,1.0537483,-2.6396675,-31.96078)">
+      <ellipse
+         style="opacity:1;fill:#ffffff;fill-opacity:1;fill-rule:nonzero;stroke:#ffffff;stroke-width:16.2743454;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1"
+         id="ellipse4169"
+         cx="105.60714"
+         cy="801.49915"
+         rx="48.358273"
+         ry="49.881367" />
+      <ellipse
+         ry="46.779255"
+         rx="45.350883"
+         cy="801.49915"
+         cx="105.60714"
+         id="path4155"
+         style="opacity:1;fill:none;fill-opacity:1;fill-rule:nonzero;stroke:#000000;stroke-width:10.60000038;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1" />
+      <path
+         sodipodi:nodetypes="ccc"
+         inkscape:connector-curvature="0"
+         id="path4157"
+         d="m 105.43939,778.58547 -0.90353,23.78989 34.99518,0.1"
+         style="fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:6.60000038;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+    </g>
+    <path
+       style="fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:8.85148525;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+       d="m 145.93884,882.62321 60.65932,0"
+       id="path4293"
+       inkscape:connector-curvature="0"
+       sodipodi:nodetypes="cc" />
+    <path
+       sodipodi:nodetypes="cc"
+       inkscape:connector-curvature="0"
+       id="path4295"
+       d="m 145.93884,910.62321 60.65932,0"
+       style="fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:8.85148525;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+    <path
+       style="fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:8.85148525;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+       d="m 145.93884,938.62321 60.65932,0"
+       id="path4297"
+       inkscape:connector-curvature="0"
+       sodipodi:nodetypes="cc" />
+    <path
+       sodipodi:nodetypes="cc"
+       inkscape:connector-curvature="0"
+       id="path4299"
+       d="m 145.93884,966.62321 60.65932,0"
+       style="fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:8.85148525;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+    <path
+       style="fill:none;fill-rule:evenodd;stroke:#000000;stroke-width:8.85148525;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+       d="m 145.93884,996.62321 60.65932,0"
+       id="path4301"
+       inkscape:connector-curvature="0"
+       sodipodi:nodetypes="cc" />
+  </g>
+</svg>

scal3/ui_gtk/event/history.py
@@ -1,0 +1,456 @@
+from collections import OrderedDict
+
+from scal3 import core
+from scal3.locale_man import tr as _
+from scal3.s_object import loadBsonObject
+from scal3 import event_lib
+from scal3.time_utils import getJhmsFromEpoch
+from scal3.json_utils import dataToPrettyJson
+from scal3.format_time import compileTmFormat
+
+from scal3 import ui
+
+from scal3.ui_gtk import *
+from scal3.ui_gtk import gtk_ud as ud
+from scal3.ui_gtk.utils import dialog_add_button
+from scal3.ui_gtk.utils import showInfo
+from scal3.ui_gtk.mywidgets.text_widgets import ReadOnlyTextView
+
+from scal3.ui_gtk.event import makeWidget
+from scal3.ui_gtk.event.utils import checkEventsReadOnly
+
+historyTimeBinFmt = compileTmFormat("%Y/%m/%d    %H:%M:%S")
+
+def _unnestStep(dst, src, path):
+	if not isinstance(src, (dict, OrderedDict)):
+		dst[path] = src
+		return
+	if path:
+		path += "."
+	for key, value in src.items():
+		_unnestStep(dst, value, path + key)
+
+def unnest(src):
+	if not isinstance(src, (dict, OrderedDict)):
+		return src
+	dst = OrderedDict()
+	_unnestStep(dst, src, "")
+	return dst
+
+
+class EventHistoryDialog(gtk.Dialog):
+	textViewTypes = [
+		"Change Table",
+		"Full Table",
+		"After change (Text)",
+		"After change (JSON)",
+		"After change (Plain JSON)",
+		"Before change (Text)",
+		"Before change (JSON)",
+		"Before change (Plain JSON)",
+		"Change (JSON Diff)",
+	]
+
+	def onResponse(self, w, e):
+		self.hide()
+		if ui.eventManDialog:
+			ui.eventManDialog.onConfigChange()
+
+	def __init__(
+		self,
+		event,
+		**kwargs
+	):
+		checkEventsReadOnly()
+		gtk.Dialog.__init__(self, **kwargs)
+		self.set_title(_("History") + ": " + event.summary)
+		self.event = event
+		self.objectCache = {} # hash(str) -> data(dict)
+
+		dialog_add_button(
+			self,
+			gtk.STOCK_CLOSE,
+			_("_Close"),
+			gtk.ResponseType.OK,
+		)
+
+		self.connect("response", self.onResponse)
+
+		treev = gtk.TreeView()
+		treev.set_headers_clickable(True)
+		trees = gtk.ListStore(
+			str, # hashBefore (hidden)
+			str, # hashAfter (hidden)
+			str, # formatted date & time
+			str, # change msg (names or the number of changed params)
+		)
+		treev.set_model(trees)
+		treev.connect("cursor-changed", self.treeviewCursorChanged)
+		# treev.connect("button-press-event", self.treeviewCursorChanged)
+		# FIXME: what is the signal for deselecting / unselecting a row?
+		self.trees = trees
+		self.treev = treev
+
+		treevSwin = gtk.ScrolledWindow()
+		treevSwin.add(treev)
+		treevSwin.set_policy(
+			gtk.PolicyType.AUTOMATIC,
+			gtk.PolicyType.AUTOMATIC,
+		)
+
+		hpan = gtk.HPaned()
+		hpan.add1(treevSwin)
+		leftVbox = gtk.VBox()
+		hpan.add2(leftVbox)
+		hpan.set_position(600)
+		pack(self.vbox, hpan, expand=True, fill=True)
+
+		actionBox = gtk.VBox(spacing=5)
+		pack(leftVbox, actionBox, padding=30)
+
+		# revertButton = gtk.Button()
+		# revertButton.set_label(_("Revert this vhange"))
+		# revertButton.set_image(gtk.Image.new_from_stock(
+		# 	gtk.STOCK_UNDO,
+		# 	gtk.IconSize.BUTTON,
+		# ))
+		# revertButton.connect("clicked", self.revertClicked)
+		# pack(actionBox, revertButton, padding=1)
+		# self.revertButton = revertButton
+
+		checkoutAfterButton = gtk.Button()
+		checkoutAfterButton.set_label(_("Select revision after this change"))
+		checkoutAfterButton.set_image(gtk.Image.new_from_stock(
+			gtk.STOCK_UNDO,
+			gtk.IconSize.BUTTON,
+		))
+		checkoutAfterButton.connect("clicked", self.checkoutAfterClicked)
+		pack(actionBox, checkoutAfterButton, padding=1)
+		self.checkoutAfterButton = checkoutAfterButton
+
+		checkoutBeforeButton = gtk.Button()
+		checkoutBeforeButton.set_label(_("Select revision before this change"))
+		checkoutBeforeButton.set_image(gtk.Image.new_from_stock(
+			gtk.STOCK_UNDO,
+			gtk.IconSize.BUTTON,
+		))
+		checkoutBeforeButton.connect("clicked", self.checkoutBeforeClicked)
+		pack(actionBox, checkoutBeforeButton, padding=1)
+		self.checkoutBeforeButton = checkoutBeforeButton
+
+		self.setButtonsSensitive(False)
+
+		combo = gtk.ComboBoxText()
+		for text in self.textViewTypes:
+			combo.append_text(_(text))
+		combo.set_active(1)
+		combo.connect("changed", self.viewTypeComboChanged)
+		self.viewTypeCombo = combo
+
+		textTypeHbox = gtk.HBox()
+		pack(textTypeHbox, gtk.Label(_("View type")+": "))
+		pack(textTypeHbox, self.viewTypeCombo)
+		pack(leftVbox, textTypeHbox)
+
+		self.textview = ReadOnlyTextView()
+		self.textview.set_wrap_mode(gtk.WrapMode.NONE)
+		self.textview.set_justification(gtk.Justification.LEFT)
+		self.textbuff = self.textview.get_buffer()
+
+		cmpTreev = gtk.TreeView()
+		cmpTreev.set_headers_clickable(True)
+		cmpTrees = gtk.ListStore(
+			str, # change symbol ("M", "+", "-", "")
+			str, # key
+			str, # old value
+			str, # new value
+		)
+		cmpTreev.set_model(cmpTrees)
+		# cmpTreev.connect("cursor-changed", self.cmpTreeviewCursorChanged)
+		self.cmpTrees = cmpTrees
+		self.cmpTreev = cmpTreev
+
+		cell = gtk.CellRendererText()
+		col = gtk.TreeViewColumn("", cell, text=0)
+		col.set_resizable(True)
+		cmpTreev.append_column(col)
+
+		cell = gtk.CellRendererText()
+		col = gtk.TreeViewColumn(_("Key"), cell, text=1)
+		col.set_resizable(True)
+		cmpTreev.append_column(col)
+
+		cell = gtk.CellRendererText()
+		col = gtk.TreeViewColumn(_("Old Value"), cell, text=2)
+		col.set_resizable(True)
+		cmpTreev.append_column(col)
+
+		cell = gtk.CellRendererText()
+		col = gtk.TreeViewColumn(_("New Value"), cell, text=3)
+		col.set_resizable(True)
+		cmpTreev.append_column(col)
+
+		leftSwin = gtk.ScrolledWindow()
+		leftSwin.set_policy(
+			gtk.PolicyType.AUTOMATIC,
+			gtk.PolicyType.AUTOMATIC,
+		)
+		pack(leftVbox, leftSwin, expand=True, fill=True)
+		self.leftSwin = leftSwin
+
+		cell = gtk.CellRendererText()
+		col = gtk.TreeViewColumn(_("Time"), cell, text=2)
+		treev.append_column(col)
+
+		cell = gtk.CellRendererText()
+		col = gtk.TreeViewColumn(_("Change Summary"), cell, text=3)
+		treev.append_column(col)
+		col.set_property("expand", True)
+
+		self.load()
+		self.vbox.show_all()
+		self.resize(ud.screenW, ud.screenH*0.9) # FIXME
+
+	def treeviewCursorChanged(self, treev, e=None):
+		self.updateViewType()
+
+	def viewTypeComboChanged(self, combo):
+		self.updateViewType()
+
+	def setButtonsSensitive(self, sensitive: bool):
+		# self.revertButton.set_sensitive(sensitive)
+		self.checkoutAfterButton.set_sensitive(sensitive)
+		self.checkoutBeforeButton.set_sensitive(sensitive)
+
+	def updateViewType(self):
+		path = self.treev.get_cursor()[0]
+		if not path:
+			self.setButtonsSensitive(False)
+			return
+		assert len(path) == 1
+		self.setButtonsSensitive(True)
+		index = path[0]
+		row = self.trees[index]
+		hashBefore = row[0]
+		hashAfter = row[1]
+
+		viewTypeIndex = self.viewTypeCombo.get_active()
+		if viewTypeIndex == -1:
+			return
+		viewType = self.textViewTypes[viewTypeIndex]
+
+		if viewType in ("Change Table", "Full Table"):
+			self.updateTableViewType(viewType, hashBefore, hashAfter)
+		else:
+			self.updateTextViewType(viewType, hashBefore, hashAfter)
+
+	def updateTableViewType(self, viewType, hashBefore, hashAfter):
+		event = self.event
+		trees = self.cmpTrees
+		trees.clear()
+
+		if viewType == "Change Table":
+			if hashBefore != "":
+				diff = self.extractChangeDiff(hashBefore, hashAfter)
+				for key in sorted(diff.keys()):
+					(valueBefore, valueAfter) = diff[key]
+					trees.append(["M", key, str(valueBefore), str(valueAfter)])
+		elif viewType == "Full Table":
+			for row in self.extractFullTable(hashBefore, hashAfter):
+				trees.append(row)
+		else:
+			raise ValueError("Unexpected viewType=%r" % viewType)
+
+		self.setScrolledWinChild(self.cmpTreev)
+
+	def updateTextViewType(self, viewType, hashBefore, hashAfter):
+		event = self.event
+		text = ""
+		if viewType == "After change (Text)":
+			if hashAfter != "":
+				text = event.getRevision(hashAfter).getInfo()
+		elif viewType == "Before change (Text)":
+			if hashBefore != "":
+				text = event.getRevision(hashBefore).getInfo()
+		elif viewType == "After change (JSON)":
+			if hashAfter != "":
+				text = dataToPrettyJson(event.getRevision(hashAfter).getData())
+		elif viewType == "After change (Plain JSON)":
+			if hashAfter != "":
+				text = dataToPrettyJson(self.getObjectData(hashAfter))
+		elif viewType == "Before change (JSON)":
+			if hashBefore != "":
+				text = dataToPrettyJson(event.getRevision(hashBefore).getData())
+		elif viewType == "Before change (Plain JSON)":
+			if hashBefore != "":
+				text = dataToPrettyJson(self.getObjectData(hashBefore))
+		elif viewType == "Change (JSON Diff)":
+			if hashBefore != "" and hashAfter != "":
+				diff = self.extractChangeDiff(hashBefore, hashAfter)
+				text = dataToPrettyJson(diff)
+		else:
+			raise ValueError("Unexpected viewType=%r" % viewType)
+		
+		self.textbuff.set_text(text)
+		self.setScrolledWinChild(self.textview)
+
+	def setScrolledWinChild(self, new_child):
+		old_child = self.leftSwin.get_child()
+		if old_child != new_child:
+			if old_child is not None:
+				print("removing old child")
+				self.leftSwin.remove(old_child)
+			print("adding new child")
+			self.leftSwin.add(new_child)
+			new_child.show()
+
+	def switchToRevision(self, revHash):
+		newEvent = self.event.getRevision(revHash)
+		self.event.parent.removeFromCache(self.event.id)
+		# newEvent.id is set
+		newEvent.parent = self.event.parent
+		newEvent.save()
+		self.event = newEvent
+		self.load()
+		ui.eventDiff.add("e", newEvent)
+
+	def checkoutAfterClicked(self, button):
+		path = self.treev.get_cursor()[0]
+		if not path:
+			return
+		assert len(path) == 1
+		index = path[0]
+		row = self.trees[index]
+		hashAfter = row[1]
+		self.switchToRevision(hashAfter)
+
+	def checkoutBeforeClicked(self, button):
+		path = self.treev.get_cursor()[0]
+		if not path:
+			return
+		assert len(path) == 1
+		index = path[0]
+		row = self.trees[index]
+		hashBefore = row[0]
+		self.switchToRevision(hashBefore)
+		
+	# def revertClicked(self, button):
+	# 	path = self.treev.get_cursor()[0]
+	# 	if not path:
+	# 		return
+	# 	assert len(path) == 1
+	# 	index = path[0]
+	# 	row = self.trees[index]
+	# 	hashBefore = row[0]
+	# 	hashAfter = row[1]
+	# 	# TODO
+
+	def formatEpoch(self, epoch):
+		jd, h, m, s = getJhmsFromEpoch(epoch)
+		cell = ui.cellCache.getCell(jd)
+		return cell.format(historyTimeBinFmt, tm=(h, m, s))
+
+	def normalizeObjectData(self, data):
+		if "rules" in data:
+			rulesOd = OrderedDict()
+			for name, value in data["rules"]:
+				rulesOd[name] = value
+			data["rules"] = rulesOd
+		return unnest(data)
+
+	# returns normalized data ("rules.RULE_NAME" keys)
+	def getObjectData(self, _hash):
+		if not _hash:
+			return {}
+		if _hash in self.objectCache:
+			return self.objectCache[_hash]
+		data = loadBsonObject(_hash)
+		data = self.normalizeObjectData(data)
+		if len(self.objectCache) > 100:
+			self.objectCache.popitem()
+		self.objectCache[_hash] = data
+		return data
+
+	def extractChangeDiff(self, hashBefore, hashAfter):
+		"""
+		returns: dict: param -> (valueBefore, valueAfter)
+		"""
+		dataBefore = self.getObjectData(hashBefore)
+		dataAfter = self.getObjectData(hashAfter)
+		diff = {}
+		for key, valueBefore in dataBefore.items():
+			valueAfter = dataAfter.get(key, None)
+			if valueAfter == valueBefore:
+				continue
+			diff[key] = (valueBefore, valueAfter)
+		for key, valueAfter in dataAfter.items():
+			if key in diff:
+				continue
+			valueBefore = dataBefore.get(key, None)
+			if valueAfter == valueBefore:
+				continue
+			diff[key] = (valueBefore, valueAfter)
+		
+		return diff
+
+	def extractFullTable(self, hashBefore, hashAfter):
+		dataBefore = self.getObjectData(hashBefore)
+		dataAfter = self.getObjectData(hashAfter)
+		dataFull = [] # (symbol, key, valueBefore, valueAfter)
+		keys = sorted(set(dataBefore.keys()).union(dataAfter.keys()))
+		for key in keys:
+			valueBefore = dataBefore.get(key, "")
+			valueAfter = dataAfter.get(key, "")
+			symbol = ""
+			if valueBefore == valueAfter:
+				pass
+			elif key not in dataBefore:
+				symbol = "+"
+			elif key not in dataAfter:
+				symbol = "-"
+			else:
+				symbol = "M"
+			dataFull.append([
+				symbol,
+				key,
+				"%s" % (valueBefore,),
+				"%s" % (valueAfter,),
+			])
+		return dataFull
+
+	def extractChangeSummary(self, diff):
+		"""
+		diff: dict: param -> (valueBefore, valueAfter)
+		"""
+
+		if len(diff) < 3:
+			return ", ".join(diff.keys())
+		
+		return _("%s parameters") % _(len(diff))
+
+	def load(self):
+		trees = self.trees
+		trees.clear()
+		hist = self.event.loadHistory()
+		count = len(hist)
+		for index, (epoch, hashAfter) in enumerate(hist):
+			if index == count-1:
+				trees.append([
+					"",
+					hashAfter,
+					self.formatEpoch(epoch),
+					_("(Added Event)"),
+				])
+				continue				
+			
+			hashBefore = hist[index+1][1]
+			diff = self.extractChangeDiff(hashBefore, hashAfter)
+			changeSummary = self.extractChangeSummary(diff)
+
+			trees.append([
+				hashBefore,
+				hashAfter,
+				self.formatEpoch(epoch),
+				changeSummary,
+			])
+

scal3/ui_gtk/event/manager.py
@@ -74,6 +74,7 @@ from scal3.ui_gtk.event.group_op import (
 )
 from scal3.ui_gtk.event.account_op import FetchRemoteGroupsDialog
 from scal3.ui_gtk.event.search_events import EventSearchWindow
+from scal3.ui_gtk.event.history import EventHistoryDialog
 
 #print("Testing translator", __file__, _("About"))
 
@@ -503,6 +504,10 @@ class EventManagerDialog(gtk.Dialog, MyDialog, ud.BaseCalObj):## FIXME
 				obj_list.append(obj_list[i - 1][obj_id])
 		return obj_list
 
+	def historyOfEventFromMenu(self, menu, path):
+		group, event = self.getObjsByPath(path)
+		EventHistoryDialog(event, parent=self).run()
+
 	def genRightClickMenu(self, path):
 		## how about multi-selection? FIXME
 		## and Select _All menu item
@@ -711,6 +716,13 @@ class EventManagerDialog(gtk.Dialog, MyDialog, ud.BaseCalObj):## FIXME
 					self.editEventFromMenu,
 					path,
 				))
+			####
+			menu.add(eventWriteImageMenuItem(
+				"History",
+				"history-24.png",
+				self.historyOfEventFromMenu,
+				path,
+			))
 			####
 			moveToItem = eventWriteMenuItem(
 				_("Move to %s") % "...",

commit 2a812f233f98f460acb458c50b9f83cc084ae501

fix non-effective event cache and non-effective removeFromCache caused by commit 295190a introduced in version 3.1.0

scal3/event_lib.py
@@ -3938,12 +3938,13 @@ class EventGroup(EventContainer):
 
 	def removeFromCache(self, eid):
 		if eid in self.eventCache:
-			return self.eventCache[eid]
+			del self.eventCache[eid]
 
 	def getEvent(self, eid):
 		if eid not in self.idList:
 			raise ValueError("%s does not contain %s" % (self, eid))
-		self.removeFromCache(eid)
+		if eid in self.eventCache:
+			return self.eventCache[eid]
 		event = EventContainer.getEvent(self, eid)
 		event.parent = self
 		event.rulesHash = event.getRulesHash()