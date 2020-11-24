808281e24c5742ba161032c21b616474ccfa34e4
Saeed Rasooli <saeed.gnu@gmail.com>
2020-11-24 18:04:10 +0330

    Event Manager: allow editing events that are in trash

--- scal3/event_lib.py
+++ scal3/event_lib.py
@@ -3633,6 +3633,18 @@ class EventContainer(BsonHistEventObj):
 		"uuid",
 	)
 
+	acceptsEventTypes = (
+		"yearly",
+		"dailyNote",
+		"task",
+		"allDayTask",
+		"weekly",
+		"monthly",
+		"lifeTime",
+		"largeScale",
+		"custom",
+	)
+
 	sortBys = (
 		# name, description, is_type_dependent
 		("calType", _("Calendar Type"), False),
@@ -3705,6 +3717,7 @@ class EventContainer(BsonHistEventObj):
 		)
 		event = classes.event.byName[data["type"]](eid)
 		event.fs = self.fs
+		event.parent = self
 		event.setData(data)
 		event.lastHash = lastHash
 		event.modified = lastEpoch
@@ -3867,17 +3880,6 @@ class EventGroupsImportResult:
 class EventGroup(EventContainer):
 	name = "group"
 	desc = _("Event Group")
-	acceptsEventTypes = (
-		"yearly",
-		"dailyNote",
-		"task",
-		"allDayTask",
-		"weekly",
-		"monthly",
-		"lifeTime",
-		"largeScale",
-		"custom",
-	)
 	canConvertTo = ()
 	actions = []  # [("Export to ICS", "exportToIcs")]
 	eventActions = []  # FIXME
@@ -4259,7 +4261,6 @@ class EventGroup(EventContainer):
 			if event is not None:
 				return event
 		event = EventContainer.getEvent(self, eid)
-		event.parent = self
 		event.rulesHash = event.getRulesHash()
 		if self.enable:
 			self.setToCache(event)
