3f0a89a924b8fb1de3954b2a389326aa496533d9
Saeed Rasooli <saeed.gnu@gmail.com>
2021-11-06 02:19:30 +0330

    update install-windows.bat

--- install-windows.bat
+++ install-windows.bat
@@ -1,10 +1,10 @@
-REM Install Python 3.7
-REM Add to PATH environment variable: Python37/ and Python37/Scripts
-REM Download latest pygi-aio from: https://sourceforge.net/projects/pygobjectwin32/files/
-REM Run pygi-aio, check these packages:
-REM - Base packages
-REM - GDK-Pixbuf
-REM - GTK+
+REM Install Python 3.8+
+REM Add to PATH environment variable: Python3X/ and Python3X/Scripts
+
+REM https://sourceforge.net/p/pygobjectwin32/tickets/58/
+REM Author of pygobjectwin32(tumagonx) on 2021-01-11:
+REM while newest mingw-w64 capable of targeting msvc 14 runtime (python 3.5-3.8)
+REM and I just tried simple stuff. I dont think I will return to this project. So no.
 
 
 REM Install other dependencies:
