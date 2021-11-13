ef407ce0d27d97f179a1bc3f6d884364e948cf79
Saeed Rasooli <saeed.gnu@gmail.com>
2021-11-13 08:11:43 +0330

    fix scal3/cal_types/hijri_test.py

--- scal3/cal_types/hijri_test.py
+++ scal3/cal_types/hijri_test.py
@@ -1,8 +1,11 @@
 from scal3.cal_types import hijri
 
-if __name__ == "__main__":
+def print_to_jd_diff():
 	for ym in hijri.monthDb.monthLenByYm:
 		y, m = divmod(ym, 12)
 		m += 1
-		log.info(hijri.to_jd(y, m, 1) - hijri.to_jd_c(y, m, 1))
+		print(hijri.to_jd(y, m, 1) - hijri.to_jd_c(y, m, 1))
+
+if __name__ == "__main__":
+	print_to_jd_diff()
 
