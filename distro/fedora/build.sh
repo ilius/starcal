0e383c31a34fc8ce5f3180e20a0884164df48c07
Saeed Rasooli <saeed.gnu@gmail.com>
2022-11-12 21:29:06 +0330

    fedora installer: add python3-setuptools to deps

--- distro/fedora/build.sh
+++ distro/fedora/build.sh
@@ -47,6 +47,7 @@ requires+=('python3-dateutil')
 requires+=('python3-psutil')
 requires+=('python3-cachetools')
 requires+=('python3-requests')
+requires+=('python3-setuptools')
 #requires+=('python3-gflags') # for google api client
 
 
