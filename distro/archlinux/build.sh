cda48d5cb743d03b70084eb53afc3de3152f4c56
Saeed Rasooli <saeed.gnu@gmail.com>
2022-11-12 14:16:30 +0330

    archlinux: add python-setuptools to dependencies

--- distro/archlinux/build.sh
+++ distro/archlinux/build.sh
@@ -45,6 +45,7 @@ depends+=('python-dateutil')
 depends+=('python-psutil')
 depends+=('python-cachetools')
 depends+=('python-requests')
+depends+=('python-setuptools')
 #depends+=('python-gflags') # for google api client
 
 
