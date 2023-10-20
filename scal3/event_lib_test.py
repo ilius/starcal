bd6c266904911829a72466acbb74d366a7ad792a
Saeed Rasooli <saeed.gnu@gmail.com>
2023-10-20 11:11:40 +0330

    update scal3/event_lib_test.py

--- scal3/event_lib_test.py
+++ scal3/event_lib_test.py
@@ -1,5 +1,5 @@
 import atexit
-import os
+import shutil
 import sys
 import tempfile
 import unittest
@@ -7,13 +7,21 @@ import unittest
 sys.path.append(".")
 
 from scal3 import event_lib
+from scal3.s_object import DefaultFileSystem
 
 myTmpDir = tempfile.mkdtemp(prefix="starcal-event_lib_test-")
-atexit.register(os.removedirs, myTmpDir)
-fs = event_lib.DefaultFileSystem(myTmpDir)
+
+def removeTempDir():
+	print(f"Removing {myTmpDir}")
+	shutil.rmtree(myTmpDir)
+
+atexit.register(removeTempDir)
+fs = DefaultFileSystem(myTmpDir)
 
 event_lib.init(fs)
+print("Loading groups")
 eventGroups = event_lib.EventGroupsHolder.load(fs)
+print("Loading trash")
 eventTrash = event_lib.EventTrash.load(fs)
 
 
