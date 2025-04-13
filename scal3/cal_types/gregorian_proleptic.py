d9f81af00a30c9fff6db0c2f82f28cacbe414e31
Saeed Rasooli <saeed.gnu@gmail.com>
2025-04-13 08:30:54 +0330

    fix and implement STARCAL_FULL_IMPORT for __import__ usages, add __all__

--- scal3/cal_types/gregorian_proleptic.py
+++ scal3/cal_types/gregorian_proleptic.py
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU Affero General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
 
+__all__ = ["desc", "getMonthLen", "jd_to", "name", "to_jd"]
+
 name = "gregorian_proleptic"
 desc = "Gregorian Proleptic"
 origLang = "en"
