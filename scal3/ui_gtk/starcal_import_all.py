fb0b259fd2a8b44d7b0fac9210e5823334e905d6
Saeed Rasooli <saeed.gnu@gmail.com>
2025-04-22 20:24:27 +0330

    move STARCAL_FULL_IMPORT codes out of scal3/ui_gtk/starcal.py also create CalObj objects and call getOptionsWidget for validation

---
+++ scal3/ui_gtk/starcal_import_all.py
@@ -0,0 +1,46 @@
+# to help with testing phase and also tell code analyzers these are imported
+
+
+def doFullImport(win):
+	import scal3.cal_types.import_all
+	import scal3.event_lib_import_all
+	import scal3.ui_gtk.event.import_all
+	from scal3.ui_gtk.event.occurrence_view import LimitedHeightDayOccurrenceView
+
+	LimitedHeightDayOccurrenceView().getOptionsWidget()
+
+	from scal3.ui_gtk.mainwin_items.dayCal import CalObj
+
+	CalObj(win).getOptionsWidget()
+
+	from scal3.ui_gtk.mainwin_items.labelBox import CalObj
+
+	CalObj(win).getOptionsWidget()
+
+	from scal3.ui_gtk.mainwin_items.monthCal import CalObj
+
+	CalObj(win).getOptionsWidget()
+
+	from scal3.ui_gtk.mainwin_items.monthPBar import CalObj
+
+	CalObj(win).getOptionsWidget()
+
+	from scal3.ui_gtk.mainwin_items.seasonPBar import CalObj
+
+	CalObj(win).getOptionsWidget()
+
+	from scal3.ui_gtk.mainwin_items.toolbar import CalObj
+
+	CalObj(win).getOptionsWidget()
+
+	from scal3.ui_gtk.mainwin_items.weekCal import CalObj
+
+	CalObj(win).getOptionsWidget()
+
+	from scal3.ui_gtk.mainwin_items.yearPBar import CalObj
+
+	CalObj(win).getOptionsWidget()
+
+	from scal3.ui_gtk.pluginsText import PluginsTextBox
+
+	CalObj(win).getOptionsWidget()
