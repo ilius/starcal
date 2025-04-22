29474d0c5e40ed88c63e27ebb019048b7b6902ee
Saeed Rasooli <saeed.gnu@gmail.com>
2025-04-22 06:32:19 +0330

    drop support for Indian National calendar

--- scal3/cal_types/import_all.py
+++ scal3/cal_types/import_all.py
@@ -1,6 +1,5 @@
 from scal3.cal_types.ethiopian import desc, getMonthLen, jd_to, name, to_jd
 from scal3.cal_types.gregorian_proleptic import desc, getMonthLen, jd_to, name, to_jd
 from scal3.cal_types.hijri import desc, getMonthLen, jd_to, name, to_jd
-from scal3.cal_types.indian_national import desc, getMonthLen, jd_to, name, to_jd
 from scal3.cal_types.jalali import desc, getMonthLen, jd_to, name, to_jd
 from scal3.cal_types.julian import desc, getMonthLen, jd_to, name, to_jd
