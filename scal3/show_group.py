0f7e98d14114ba558e0799771565b164cddd9344
Saeed Rasooli <saeed.gnu@gmail.com>
2025-04-23 13:35:38 +0330

    rename funcs with Bson in their name
    sed -i s/updateBasicDataFromBson/updateBinaryObjectBasicData/g s_object.py event_lib/*.py
    sed -i s/loadBsonObject/loadBinaryObject/g show_event.py event_lib/*.py ui_gtk/event/history.py show_object.py show_group.py
    sed -i s/saveBsonObject/saveBinaryObject/g

--- scal3/show_group.py
+++ scal3/show_group.py
@@ -6,7 +6,7 @@ import sys
 from scal3 import event_lib
 from scal3.json_utils import dataToPrettyJson
 from scal3.path import confDir
-from scal3.s_object import loadBsonObject
+from scal3.s_object import loadBinaryObject
 
 if __name__ == "__main__":
 	fs = event_lib.DefaultFileSystem(confDir)
@@ -15,5 +15,5 @@ if __name__ == "__main__":
 		eventJsonData = json.load(fp)
 
 	lastHash = eventJsonData["history"][0][1]
-	data = loadBsonObject(lastHash, fs)
+	data = loadBinaryObject(lastHash, fs)
 	print(dataToPrettyJson(data))  # noqa: T201
