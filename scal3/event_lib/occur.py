17c50c1e2a98ef197e735ca41a433186b8e8b6d3
Saeed Rasooli <saeed.gnu@gmail.com>
2025-04-23 12:06:09 +0330

    break up event_lib

---
+++ scal3/event_lib/occur.py
@@ -0,0 +1,269 @@
+#
+# Copyright (C) Saeed Rasooli <saeed.gnu@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+
+from __future__ import annotations
+
+from scal3 import logger
+
+log = logger.get()
+
+
+from scal3.interval_utils import intersectionOfTwoIntervalList
+
+# from scal3.interval_utils import
+from scal3.s_object import SObj
+from scal3.time_utils import (
+	getEpochFromJd,
+	getJdFromEpoch,
+	getJdListFromEpochRange,
+)
+
+__all__ = ["IntervalOccurSet", "JdOccurSet", "OccurSet", "TimeListOccurSet"]
+
+
+class OccurSet(SObj):
+	def __init__(self) -> None:
+		self.event = None
+
+	def intersection(self) -> None:
+		raise NotImplementedError
+
+	def getDaysJdList(self) -> list[int]:  # noqa: PLR6301
+		return []  # make generator FIXME
+
+	def getTimeRangeList(self) -> list[tuple[int, int]]:  # noqa: PLR6301
+		return []  # make generator FIXME
+
+	def getStartJd(self) -> int:
+		raise NotImplementedError
+
+	def getEndJd(self) -> int:
+		raise NotImplementedError
+
+	# def __iter__(self) -> Iterator:
+	# 	return iter(self.getTimeRangeList())
+
+
+class JdOccurSet(OccurSet):
+	name = "jdSet"
+
+	def __init__(self, jdSet: set[int] | None = None) -> None:
+		OccurSet.__init__(self)
+		if not jdSet:
+			jdSet = []
+		self.jdSet = set(jdSet)
+
+	def __repr__(self) -> str:
+		return f"JdOccurSet({list(self.jdSet)})"
+
+	def __bool__(self) -> bool:
+		return bool(self.jdSet)
+
+	def __len__(self) -> int:
+		return len(self.jdSet)
+
+	def getStartJd(self) -> int:
+		if not self.jdSet:
+			return
+		return min(self.jdSet)
+
+	def getEndJd(self) -> int:
+		if not self.jdSet:
+			return
+		return max(self.jdSet) + 1
+
+	def intersection(self, occur: OccurSet) -> OccurSet:
+		if isinstance(occur, JdOccurSet):
+			return JdOccurSet(
+				self.jdSet.intersection(occur.jdSet),
+			)
+		if isinstance(occur, IntervalOccurSet):
+			return IntervalOccurSet(
+				intersectionOfTwoIntervalList(
+					self.getTimeRangeList(),
+					occur.getTimeRangeList(),
+				),
+			)
+		if isinstance(occur, TimeListOccurSet):
+			return occur.intersection(self)
+
+		raise TypeError
+
+	def getDaysJdList(self) -> list[int]:
+		return sorted(self.jdSet)
+
+	def getTimeRangeList(self) -> list[tuple[int, int]]:
+		return [
+			(
+				getEpochFromJd(jd),
+				getEpochFromJd(jd + 1),
+			)
+			for jd in self.jdSet
+		]
+
+	def calcJdRanges(self) -> list[tuple[int, int]]:
+		jdList = sorted(self.jdSet)  # jdList is sorted
+		if not jdList:
+			return []
+		startJd = jdList[0]
+		endJd = startJd + 1
+		jdRanges = []
+		for jd in jdList[1:]:
+			if jd == endJd:
+				endJd += 1
+			else:
+				jdRanges.append((startJd, endJd))
+				startJd = jd
+				endJd = startJd + 1
+		jdRanges.append((startJd, endJd))
+		return jdRanges
+
+
+class IntervalOccurSet(OccurSet):
+	name = "timeRange"
+
+	def __init__(self, rangeList: list[tuple[int, int]] | None = None) -> str:
+		OccurSet.__init__(self)
+		if not rangeList:
+			rangeList = []
+		self.rangeList = rangeList
+
+	def __repr__(self) -> str:
+		return f"IntervalOccurSet({self.rangeList!r})"
+
+	def __bool__(self) -> bool:
+		return bool(self.rangeList)
+
+	def __len__(self) -> int:
+		return len(self.rangeList)
+
+	# def __getitem__(i):
+	# 	self.rangeList.__getitem__(i)  # FIXME
+
+	def getStartJd(self) -> int:
+		if not self.rangeList:
+			return
+		return getJdFromEpoch(min(r[0] for r in self.rangeList))
+
+	def getEndJd(self) -> int:
+		if not self.rangeList:
+			return
+		return getJdFromEpoch(max(r[1] for r in self.rangeList))
+
+	def intersection(self, occur: OccurSet) -> OccurSet:
+		if isinstance(occur, JdOccurSet | IntervalOccurSet):
+			return IntervalOccurSet(
+				intersectionOfTwoIntervalList(
+					self.getTimeRangeList(),
+					occur.getTimeRangeList(),
+				),
+			)
+		if isinstance(occur, TimeListOccurSet):
+			return occur.intersection(self)
+
+		raise TypeError(
+			f"bad type {occur.__class__.__name__} ({occur!r})",
+		)
+
+	def getDaysJdList(self) -> list[int]:
+		return sorted(
+			{
+				jd
+				for startEpoch, endEpoch in self.rangeList
+				for jd in getJdListFromEpochRange(startEpoch, endEpoch)
+			},
+		)
+
+	def getTimeRangeList(self) -> list[tuple[int, int]]:
+		return self.rangeList
+
+	@staticmethod
+	def newFromStartEnd(startEpoch: int, endEpoch: int) -> OccurSet:
+		if startEpoch > endEpoch:
+			return IntervalOccurSet([])
+		return IntervalOccurSet([(startEpoch, endEpoch)])
+
+
+class TimeListOccurSet(OccurSet):
+	name = "repeativeTime"
+
+	def __init__(self, *args) -> None:
+		OccurSet.__init__(self)
+		if not args:
+			self.startEpoch = 0
+			self.endEpoch = 0
+			self.stepSeconds = -1
+			self.epochList = set()
+		elif len(args) == 1:
+			self.epochList = set(args[0])
+		elif len(args) == 3:
+			self.setRange(*args)
+		else:
+			raise ValueError
+
+	def __repr__(self) -> str:
+		return r"TimeListOccurSet({self.epochList!r})"
+
+	# def __bool__(self) -> bool:
+	# 	return self.startEpoch == self.endEpoch
+
+	def __bool__(self) -> bool:
+		return bool(self.epochList)
+
+	def getStartJd(self) -> int:
+		if not self.epochList:
+			return
+		return getJdFromEpoch(min(self.epochList))
+
+	def getEndJd(self) -> int:
+		if not self.epochList:
+			return
+		return getJdFromEpoch(max(self.epochList) + 1)
+
+	def setRange(self, startEpoch: int, endEpoch: int, stepSeconds: int) -> None:
+		try:
+			from numpy.core.multiarray import arange
+		except ImportError:
+			from scal3.utils import arange
+		# ------
+		self.startEpoch = startEpoch
+		self.endEpoch = endEpoch
+		self.stepSeconds = stepSeconds
+		self.epochList = set(arange(startEpoch, endEpoch, stepSeconds))
+
+	def intersection(self, occur: OccurSet) -> OccurSet:
+		if isinstance(occur, JdOccurSet | IntervalOccurSet):
+			epochBetween = []
+			for epoch in self.epochList:
+				for startEpoch, endEpoch in occur.getTimeRangeList():
+					if startEpoch <= epoch < endEpoch:
+						epochBetween.append(epoch)
+						break
+			return TimeListOccurSet(epochBetween)
+
+		if isinstance(occur, TimeListOccurSet):
+			return TimeListOccurSet(
+				self.epochList.intersection(occur.epochList),
+			)
+
+		raise TypeError
+
+	# FIXME: improve performance
+	def getDaysJdList(self) -> list[int]:
+		return sorted({getJdFromEpoch(epoch) for epoch in self.epochList})
+
+	def getTimeRangeList(self) -> list[tuple[int, int]]:
+		return [(epoch, epoch) for epoch in self.epochList]  # or end=None, FIXME
---
+++ scal3/event_lib/occur_data.py
@@ -0,0 +1,363 @@
+#
+# Copyright (C) Saeed Rasooli <saeed.gnu@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+
+from __future__ import annotations
+
+from scal3 import logger
+
+log = logger.get()
+
+
+import operator
+from typing import TYPE_CHECKING, NamedTuple
+
+from scal3 import core
+from scal3.cal_types import calTypes
+from scal3.core import jd_to_primary
+from scal3.date_utils import getJdRangeForMonth
+from scal3.time_utils import (
+	HMS,
+	getEpochFromJd,
+	getJhmsFromEpoch,
+)
+
+from .occur import IntervalOccurSet, JdOccurSet, TimeListOccurSet
+
+if TYPE_CHECKING:
+	from .events import Event
+	from .groups import EventGroup
+
+__all__ = ["getDayOccurrenceData", "getWeekOccurrenceData"]
+dayLen = 24 * 3600
+hms_zero = HMS()
+hms_24 = HMS(24)
+keyFuncIndex0 = operator.itemgetter(0)
+
+
+class DayOccurData(NamedTuple):
+	time: str
+	time_epoch: tuple[int, int]
+	is_allday: bool
+	text: str
+	icon: str
+	color: tuple[int, int, int]
+	ids: tuple[int, int]
+	show: tuple[bool, bool, bool]  # (showInDCal, showInWCal, showInMCal)
+	showInStatusIcon: bool
+
+
+class WeekOccurData(NamedTuple):
+	weekDay: int
+	time: str
+	text: str
+	icon: str
+
+
+class MonthOccurData(NamedTuple):
+	day: int
+	time: str
+	text: str
+	icon: str
+	ids: tuple[int, int]
+
+
+def getDayOccurrenceData(curJd, groups, tfmt="HM$"):
+	data = []
+	for groupIndex, group in enumerate(groups):
+		if not group.enable:
+			continue
+		if not group.showInCal():
+			continue
+		# log.debug("\nupdateData: checking event", event.summary)
+		gid = group.id
+		color = group.color
+		for item in group.occur.search(
+			getEpochFromJd(curJd),
+			getEpochFromJd(curJd + 1),
+		):
+			eid = item.eid
+			epoch0 = item.start
+			epoch1 = item.end
+			event = group[eid]
+			# ---
+			text = event.getTextParts()
+			# ---
+			timeStr = ""
+			if epoch1 - epoch0 < dayLen:
+				jd0, hms0 = getJhmsFromEpoch(epoch0)
+				if jd0 < curJd:
+					hms0 = hms_zero
+				if epoch1 - epoch0 < 1:
+					timeStr = f"{hms0:{tfmt}}"
+				else:
+					jd1, hms1 = getJhmsFromEpoch(epoch1)
+					if jd1 > curJd:
+						hms1 = hms_24
+					timeStr = f"{hms0:{tfmt}} - {hms1:{tfmt}}"
+			# ---
+			try:
+				eventIndex = group.index(eid)
+			except ValueError:
+				eventIndex = event.modified  # FIXME
+			data.append(
+				(
+					(epoch0, epoch1, groupIndex, eventIndex),  # FIXME for sorting
+					DayOccurData(
+						time=timeStr,
+						time_epoch=(epoch0, epoch1),
+						is_allday=epoch0 % dayLen + epoch1 % dayLen == 0,
+						text=text,
+						icon=event.getIconRel(),
+						color=color,
+						ids=(gid, eid),
+						show=(
+							group.showInDCal,
+							group.showInWCal,
+							group.showInMCal,
+						),
+						showInStatusIcon=group.showInStatusIcon,
+					),
+				),
+			)
+	data.sort(key=keyFuncIndex0)
+	return [item[1] for item in data]
+
+
+def getWeekOccurrenceData(curAbsWeekNumber, groups, tfmt="HM$"):
+	startJd = core.getStartJdOfAbsWeekNumber(curAbsWeekNumber)
+	endJd = startJd + 7
+	data = []
+
+	def add(group: EventGroup, event: Event, eData: dict):
+		eData["show"] = (
+			group.showInDCal,
+			group.showInWCal,
+			group.showInMCal,
+		)
+		eData["ids"] = (group.id, event.id)
+		data.append(eData)
+
+	def handleEvent(event, group):
+		occur = event.calcEventOccurrenceIn(startJd, endJd)
+		if not occur:
+			return
+		text = event.getText()
+		icon = event.getIconRel()
+		if isinstance(occur, JdOccurSet):
+			for jd in occur.getDaysJdList():
+				wnum, weekDay = core.getWeekDateFromJd(jd)
+				if wnum != curAbsWeekNumber:
+					continue
+				add(
+					group,
+					event,
+					WeekOccurData(
+						weekDay=weekDay,
+						time="",
+						text=text,
+						icon=icon,
+					),
+				)
+		elif isinstance(occur, IntervalOccurSet):
+			for startEpoch, endEpoch in occur.getTimeRangeList():
+				jd1, hms1 = getJhmsFromEpoch(startEpoch)
+				jd2, hms2 = getJhmsFromEpoch(endEpoch)
+				wnum, weekDay = core.getWeekDateFromJd(jd1)
+				if wnum != curAbsWeekNumber:
+					continue
+				if jd1 == jd2:
+					add(
+						group,
+						event,
+						WeekOccurData(
+							weekDay=weekDay,
+							time=f"{hms1:{tfmt}} - {hms2:{tfmt}}",
+							text=text,
+							icon=icon,
+						),
+					)
+					continue
+				# FIXME
+				add(
+					group,
+					event,
+					WeekOccurData(
+						weekDay=weekDay,
+						time=f"{hms1:{tfmt}} - {hms_24:{tfmt}}",
+						text=text,
+						icon=icon,
+					),
+				)
+				for jd in range(jd1 + 1, jd2):
+					wnum, weekDay = core.getWeekDateFromJd(jd)
+					if wnum != curAbsWeekNumber:
+						break
+					add(
+						group,
+						event,
+						WeekOccurData(
+							weekDay=weekDay,
+							time="",
+							text=text,
+							icon=icon,
+						),
+					)
+
+				wnum, weekDay = core.getWeekDateFromJd(jd2)
+				if wnum != curAbsWeekNumber:
+					continue
+				add(
+					group,
+					event,
+					WeekOccurData(
+						weekDay=weekDay,
+						time=f"{hms_zero:{tfmt}} - {hms2:{tfmt}}",
+						text=text,
+						icon=icon,
+					),
+				)
+		elif isinstance(occur, TimeListOccurSet):
+			for epoch in occur.epochList:
+				jd, hms = getJhmsFromEpoch(epoch)
+				wnum, weekDay = core.getWeekDateFromJd(jd)
+				if wnum != curAbsWeekNumber:
+					continue
+				add(
+					group,
+					event,
+					WeekOccurData(
+						weekDay=weekDay,
+						time=f"{hms:{tfmt}}",
+						text=text,
+						icon=icon,
+					),
+				)
+		else:
+			raise TypeError
+
+	for group in groups:
+		if not group.enable:
+			continue
+		for event in group:
+			if not event:
+				continue
+			handleEvent(event, group)
+
+	return data
+
+
+def getMonthOccurrenceData(curYear, curMonth, groups, tfmt="HM$"):
+	startJd, endJd = getJdRangeForMonth(curYear, curMonth, calTypes.primary)
+	data = []
+
+	def handleEvent(event, group):
+		occur = event.calcEventOccurrenceIn(startJd, endJd)
+		if not occur:
+			return
+		text = event.getText()
+		icon = event.getIconRel()
+		ids = (group.id, event.id)
+		if isinstance(occur, JdOccurSet):
+			for jd in occur.getDaysJdList():
+				y, m, d = jd_to_primary(jd)
+				if y == curYear and m == curMonth:
+					data.append(
+						MonthOccurData(
+							day=d,
+							time="",
+							text=text,
+							icon=icon,
+							ids=ids,
+						),
+					)
+		elif isinstance(occur, IntervalOccurSet):
+			for startEpoch, endEpoch in occur.getTimeRangeList():
+				jd1, hms1 = getJhmsFromEpoch(startEpoch)
+				jd2, hms2 = getJhmsFromEpoch(endEpoch)
+				y, m, d = jd_to_primary(jd1)
+				if not (y == curYear and m == curMonth):
+					continue
+				if jd1 == jd2:
+					data.append(
+						MonthOccurData(
+							day=d,
+							time=f"{hms1:{tfmt}} - {hms2:{tfmt}}",
+							text=text,
+							icon=icon,
+							ids=ids,
+						),
+					)
+					continue
+				# FIXME
+				data.append(
+					MonthOccurData(
+						day=d,
+						time=f"{hms1:{tfmt}} - {hms_24:{tfmt}}",
+						text=text,
+						icon=icon,
+						ids=ids,
+					),
+				)
+				for jd in range(jd1 + 1, jd2):
+					y, m, d = jd_to_primary(jd)
+					if y == curYear and m == curMonth:
+						data.append(
+							MonthOccurData(
+								day=d,
+								time="",
+								text=text,
+								icon=icon,
+								ids=ids,
+							),
+						)
+					else:
+						break
+				y, m, d = jd_to_primary(jd2)
+				if y == curYear and m == curMonth:
+					data.append(
+						MonthOccurData(
+							day=d,
+							time=f"{hms_zero:{tfmt}} - {hms2:{tfmt}}",
+							text=text,
+							icon=icon,
+							ids=ids,
+						),
+					)
+		elif isinstance(occur, TimeListOccurSet):
+			for epoch in occur.epochList:
+				jd, hms = getJhmsFromEpoch(epoch)
+				y, m, d = jd_to_primary(jd1)
+				if y == curYear and m == curMonth:
+					data.append(
+						MonthOccurData(
+							day=d,
+							time=f"{hms:{tfmt}}",
+							text=text,
+							icon=icon,
+							ids=ids,
+						),
+					)
+		else:
+			raise TypeError
+
+	for group in groups:
+		if not group.enable:
+			continue
+		for event in group:
+			if not event:
+				continue
+			handleEvent(event, group)
+	return data
