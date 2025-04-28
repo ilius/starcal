93f788e6b51239b5786e84dab31ad9372fab96dc
Saeed Rasooli <saeed.gnu@gmail.com>
2025-04-28 05:50:08 +0330

    Jalali calendar: remove 2820-year algorithm along with the option/config

--- scal3/cal_types/jalali.py
+++ scal3/cal_types/jalali.py
@@ -31,23 +31,13 @@ name = "jalali"
 desc = "Jalali"
 origLang = "fa"
 
-ALG33 = 0
-ALG2820 = 1
-
 monthNameMode = 0
-jalaliAlg = 0
 options = (
 	(
 		"monthNameMode",
 		list,
 		"Jalali Month Names",
 		("Iranian", "Kurdish/Maadi", "Afghan/Dari", "Pashto"),
-	),
-	(
-		"jalaliAlg",
-		list,
-		"Jalali Calculation Algorithm",
-		("33 year algorithm", "2820 year algorithm"),
 	),
 )
 
@@ -177,7 +167,7 @@ from scal3.config_utils import (
 )
 from scal3.path import confDir, sysConfDir
 
-# Here load user options(jalaliAlg) from file
+# Here load user options from file
 sysConfPath = f"{sysConfDir}/{name}.json"
 loadSingleConfig(__name__, sysConfPath)
 
@@ -191,28 +181,16 @@ def save():
 	saveSingleConfig(
 		__name__,
 		confPath,
-		(
-			"monthNameMode",
-			"jalaliAlg",
-		),
+		("monthNameMode",),
 	)
 
 
 def isLeap(year):
 	"""isLeap: Is a given year a leap year in the Jalali calendar ?."""
-	alg = jalaliAlg
-	if alg == ALG2820:
-		# originally: year - 473 - (year > -2)
-		# since we don't exclude Zero Year, this seems like the correct formula:
-		return (((year - 474) % 2820) * 682) % 2816 < 682
-
-	if alg == ALG33:
-		jy = year - 979
-		jyd, jym = divmod(jy, 33)
-		jyd2, jym2 = divmod(jy + 1, 33)
-		return ((jyd2 - jyd) * 8 + (jym2 + 3) // 4 - (jym + 3) // 4) == 1
-
-	raise RuntimeError(f"bad option {alg=}")
+	jy = year - 979
+	jyd, jym = divmod(jy, 33)
+	jyd2, jym2 = divmod(jy + 1, 33)
+	return ((jyd2 - jyd) * 8 + (jym2 + 3) // 4 - (jym + 3) // 4) == 1
 
 
 def getMonthDayFromYdays(yday):
@@ -223,70 +201,35 @@ def getMonthDayFromYdays(yday):
 
 def to_jd(year, month, day):
 	"""Calculate Julian day from Jalali date."""
-	alg = jalaliAlg
-	if alg == ALG2820:
-		# originally: year - (474 if year >= 0 else 473)
-		# since we don't exclude Zero Year, this seems like the correct formula:
-		epbase = year - 474
-		epbase_d, epbase_m = divmod(epbase, 2820)
-		epyear = 474 + epbase_m
-		return (
-			day
-			+ (month - 1) * 30
-			+ min(6, month - 1)
-			+ (epyear * 682 - 110) // 2816
-			+ (epyear - 1) * 365
-			+ epbase_d * 1029983
-			+ epoch
-			- 1
-		)
-
-	if alg == ALG33:
-		jy = year - 979
-		jyd, jym = divmod(jy, 33)
-		return (
-			365 * jy
-			+ jyd * 8
-			+ (jym + 3) // 4
-			+ monthLenSum[month - 1]
-			+ day
-			- 1
-			+ 584101
-			+ GREGORIAN_EPOCH
-		)
-
-	raise RuntimeError(f"bad option {alg=}")
+	jy = year - 979
+	jyd, jym = divmod(jy, 33)
+	return (
+		365 * jy
+		+ jyd * 8
+		+ (jym + 3) // 4
+		+ monthLenSum[month - 1]
+		+ day
+		- 1
+		+ 584101
+		+ GREGORIAN_EPOCH
+	)
 
 
 def jd_to(jd):
 	"""Calculate Jalali date from Julian day."""
-	alg = jalaliAlg
-	if alg == ALG2820:
-		cycle, cyear = divmod(jd - to_jd(475, 1, 1), 1029983)
-		if cyear == 1029982:
-			ycycle = 2820
-		else:
-			aux1, aux2 = divmod(cyear, 366)
-			ycycle = (2134 * aux1 + 2816 * aux2 + 2815) // 1028522 + aux1 + 1
-		year = 2820 * cycle + ycycle + 474
-		yday = jd - to_jd(year, 1, 1) + 1
-		month, day = getMonthDayFromYdays(yday)
-	elif alg == ALG33:
-		jdays = int(jd - GREGORIAN_EPOCH - 584101)
-		# -(1600*365 + 1600//4 - 1600//100 + 1600//400) + 365-79+1 == -584101
-		# log.debug("jdays =", jdays)
-		j_np, jdays = divmod(jdays, 12053)
+	jdays = int(jd - GREGORIAN_EPOCH - 584101)
+	# -(1600*365 + 1600//4 - 1600//100 + 1600//400) + 365-79+1 == -584101
+	# log.debug("jdays =", jdays)
+	j_np, jdays = divmod(jdays, 12053)
 
-		yearFact, jdays = divmod(jdays, 1461)
-		year = 979 + 33 * j_np + 4 * yearFact
+	yearFact, jdays = divmod(jdays, 1461)
+	year = 979 + 33 * j_np + 4 * yearFact
 
-		if jdays >= 366:
-			yearPlus, jdays = divmod(jdays - 1, 365)
-			year += yearPlus
-		yday = jdays + 1
-		month, day = getMonthDayFromYdays(yday)
-	else:
-		raise RuntimeError(f"bad option {alg=}")
+	if jdays >= 366:
+		yearPlus, jdays = divmod(jdays - 1, 365)
+		year += yearPlus
+	yday = jdays + 1
+	month, day = getMonthDayFromYdays(yday)
 	return year, month, day
 
 
