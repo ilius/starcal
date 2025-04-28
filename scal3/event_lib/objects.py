5370a891366393664034d3c6f791cddb8cea5dd8
Saeed Rasooli <saeed.gnu@gmail.com>
2025-04-28 19:50:03 +0330

    move iterObjectFiles to event_lib.objects

--- scal3/event_lib/objects.py
+++ scal3/event_lib/objects.py
@@ -20,14 +20,22 @@ from scal3 import logger
 
 log = logger.get()
 
+from os.path import join
+from typing import TYPE_CHECKING
 
 from scal3.event_lib import state
 from scal3.s_object import (
 	SObjBinaryModel,
 	SObjTextModel,
+	objectDirName,
 )
 
-__all__ = ["EventObjTextModel", "HistoryEventObjBinaryModel"]
+if TYPE_CHECKING:
+	from collections.abc import Iterable
+
+	from scal3.filesystem import FileSystem
+
+__all__ = ["EventObjTextModel", "HistoryEventObjBinaryModel", "iterObjectFiles"]
 
 
 class EventObjTextModel(SObjTextModel):
@@ -54,3 +62,29 @@ class HistoryEventObjBinaryModel(SObjBinaryModel):
 		if hasattr(self, "uuid") and self.uuid is None:
 			self.set_uuid()
 		return SObjBinaryModel.save(self, *args)
+
+
+def iterObjectFiles(fs: FileSystem) -> Iterable[tuple[str, str]]:
+	for dname in fs.listdir(objectDirName):
+		dpath = join(objectDirName, dname)
+		if not fs.isdir(dpath):
+			continue
+		if len(dname) != 2:
+			if dname.startswith("."):
+				continue
+			log.error(f"Unexpected directory: {dname}")  # do not skip it!
+		for fname in fs.listdir(dpath):
+			fpath = join(dpath, fname)
+			if not fs.isfile(fpath):
+				log.error(f"Object file does not exist or not a file: {fpath}")
+				continue
+			hash_ = dname + fname
+			if len(hash_) != 40:
+				log.debug(f"Skipping non-object file {fpath}")
+				continue
+			try:
+				int(hash_, 16)
+			except ValueError:
+				log.debug(f"Skipping non-object file {fpath}  (not hexadecimal)")
+				continue
+			yield hash_, fpath
