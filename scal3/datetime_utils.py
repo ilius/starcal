4bc4ce2d00d8cdce3c1d698149c79b3e512f6144
Saeed Rasooli <saeed.gnu@gmail.com>
2025-04-29 19:28:55 +0330

    move epochDateTimeEncode out of core.py

---
+++ scal3/datetime_utils.py
@@ -0,0 +1,10 @@
+from scal3.cal_types import calTypes, jd_to
+from scal3.date_utils import dateEncode
+from scal3.time_utils import getJhmsFromEpoch
+
+__all__ = ["epochDateTimeEncode"]
+
+
+def epochDateTimeEncode(epoch: int) -> str:
+	jd, hms = getJhmsFromEpoch(epoch)
+	return dateEncode(jd_to(jd, calTypes.primary)) + f" {hms:HMS}"
