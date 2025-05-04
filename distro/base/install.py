f6095603cd4e17c398d847c34c0d3690281555d1
Saeed Rasooli <saeed.gnu@gmail.com>
2025-05-05 02:16:14 +0330

    distro/base/install.py: fix typos, format, add types

--- distro/base/install.py
+++ distro/base/install.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-import os
-import sys
-import subprocess
-from os.path import (
-	join,
-	dirname,
-	abspath,
-	realpath,
-	isfile,
-	isdir,
-	islink,
-)
 import getopt
+import os
 import shutil
+import subprocess
+import sys
+from os.path import (
+	abspath,
+	dirname,
+	isdir,
+	isfile,
+	islink,
+	join,
+	realpath,
+)
 from shutil import rmtree
 
 # realpath gives the target of sym link (unlike abspath)
@@ -24,28 +24,26 @@ pkgName = "starcal3"
 iconName = "starcal32.png"
 
 
-def printAsError(msg):
+def printAsError(msg: str) -> None:
 	sys.stderr.write(msg + "\n")
 	sys.stderr.flush()
 
 
-def printUsage():
-	## echo "Usage: $0 [TERGET_DIR] [--for-pkg|--portable] [--prefix=/usr/local]"
+def printUsage() -> None:
+	# echo "Usage: $0 [TERGET_DIR] [--for-pkg|--portable] [--prefix=/usr/local]"
 	U = "\x1b[4m"  # Start Underline # len=4
 	E = "\x1b[0;0;0m"  # End Format # len=8
 	print(
 		f"Usage: python3 {sys.argv[0]} [{U}TERGET_DIR{E}] "
-		f"[--for-pkg|--portable] "
+		"[--for-pkg|--portable] "
 		f"[--prefix={U}/usr/local{E}] "
-		f"[--python={U}python3.x{E}]"
+		f"[--python={U}python3.x{E}]",
 	)
 
-def getVersion():
-	outputB, error = subprocess.Popen(
-		[
-			"python3",
-			f"{sourceDir}/scripts/version.py"
-		],
+
+def getVersion() -> str:
+	outputB, _error = subprocess.Popen(
+		["python3", f"{sourceDir}/scripts/version.py"],
 		stdout=subprocess.PIPE,
 	).communicate()
 	# if error == None:
@@ -61,11 +59,12 @@ installTypeValues = [
 ]
 
 
-def makeDir(direc):
+def makeDir(direc: str) -> None:
 	if not isdir(direc):
 		os.makedirs(direc)
 
-def cleanup(cpath):
+
+def cleanup(cpath: str) -> None:
 	try:
 		if isfile(cpath):
 			os.remove(cpath)
@@ -76,46 +75,63 @@ def cleanup(cpath):
 
 
 def lsb_release() -> str:
-	outputB, error = subprocess.Popen(
+	outputB, errorB = subprocess.Popen(
 		["lsb_release", "-is"],
 		stdout=subprocess.PIPE,
+		stderr=subprocess.PIPE,
 	).communicate()
-	if error:
-		printAsError(error)
+	if errorB:
+		printAsError(errorB.decode("utf-8"))
 	return outputB.decode("utf-8").strip()
 
-def checkOperatingSystem(installType):
+
+def checkOperatingSystem(installType: str) -> bool:
 	if installType == "for-pkg":
 		return True
 
 	if isfile("/etc/debian_version"):
 		if lsb_release() == "Ubuntu":
-			printAsError("Your distribution is based on Ubuntu, use: sudo ./distro/ubuntu/install.sh")
+			printAsError(
+				"Your distribution is based on Ubuntu, "
+				"use: sudo ./distro/ubuntu/install.sh",
+			)
 		else:
-			printAsError("Your distribution is based on Debian, use: sudo ./distro/debian/install.sh")
+			printAsError(
+				"Your distribution is based on Debian, "
+				"use: sudo ./distro/debian/install.sh",
+			)
 		return False
 
-	elif isfile("/etc/SUSE-brand") or isfile("/etc/products.d/openSUSE.prod"):
-		printAsError("Your distribution is based on SUSE, use: sudo ./distro/suse/install.sh")
+	if isfile("/etc/SUSE-brand") or isfile("/etc/products.d/openSUSE.prod"):
+		printAsError(
+			"Your distribution is based on SUSE, use: sudo ./distro/suse/install.sh",
+		)
 		return False
 
-	elif isfile("/etc/fedora-release"):
-		printAsError("Your distribution is based on Red Hat, use: sudo ./distro/fedora/install.sh")
+	if isfile("/etc/fedora-release"):
+		printAsError(
+			"Your distribution is based on Red Hat, "
+			"use: sudo ./distro/fedora/install.sh",
+		)
 		return False
 
-	elif isfile("/etc/arch-release"):
-		printAsError("Your distribution is based on ArchLinux, use ./distro/archlinux/install.sh")
+	if isfile("/etc/arch-release"):
+		printAsError(
+			"Your distribution is based on ArchLinux, "
+			"use ./distro/archlinux/install.sh",
+		)
 		return False
 
 	return True
 
 
-def main():
+def main() -> int | None:
 	try:
 		(rawOptions, args) = getopt.gnu_getopt(
 			sys.argv[1:],
 			"",
-			installTypeValues + [
+			installTypeValues
+			+ [
 				"help",
 				"prefix=",
 				"python=",
@@ -126,10 +142,7 @@ def main():
 		printUsage()
 		return 1
 
-	options = {
-		opt.lstrip("-"): value
-		for opt, value in rawOptions
-	}
+	options = {opt.lstrip("-"): value for opt, value in rawOptions}
 	# print(options)
 	# print(args)
 
@@ -150,17 +163,18 @@ def main():
 				printAsError(f"can not pass both {installType} and {value}")
 				return 1
 			installType = value
+	installType = installType or "system"
 	# print(f"installType = {installType}")
 
-	# if not checkOperatingSystem(installType):
-	# 	return 1
+	if not checkOperatingSystem(installType):
+		return 1
 
 	prefix = ""
 	if "prefix" in options:
 		prefix = options["prefix"].rstrip("/")
 	elif installType == "for-pkg":
 		prefix = "/usr"
-	elif installType == "portable":
+	elif installType == "portable":  # noqa: SIM114
 		prefix = "/usr/local"
 	elif installType == "system":
 		prefix = "/usr/local"
@@ -171,19 +185,15 @@ def main():
 	pyCmd = options.get("python", "")
 	if pyCmd:
 		if not isfile(pyCmd):
-			printAsError(
-				f"The given Python executable {pyCmd!r} does not exist"
-			)
+			printAsError(f"The given Python executable {pyCmd!r} does not exist")
 	else:
 		pyCmd = sys.executable
 		userPyCmd = shutil.which("python3")
 		if not userPyCmd:
-			printAsError(
-				f"WARNING: Python executable {pyCmd!r} is not in your $PATH"
-			)
+			printAsError(f"WARNING: Python executable {pyCmd!r} is not in your $PATH")
 		elif pyCmd != userPyCmd:
 			printAsError(
-				f"WARNING: Python executable {pyCmd!r} is different from {userPyCmd!r}"
+				f"WARNING: Python executable {pyCmd!r} is different from {userPyCmd!r}",
 			)
 	print(f"Using {pyCmd}")
 
@@ -214,11 +224,8 @@ def main():
 	if subprocess.call(join(targetCodeDir, "update-perm")) != 0:
 		return 1
 
-	for docFile in ("LICENSE", "authors", "donate"):
-		os.rename(
-			join(targetCodeDir, docFile),
-			join(shareDir, "doc", pkgName, docFile)
-		)
+	for docFile in ("LICENSE", "authors"):
+		os.rename(join(targetCodeDir, docFile), join(shareDir, "doc", pkgName, docFile))
 
 	makeDir(join(shareDir, "icons", "hicolor"))
 	for size in (16, 22, 24, 32, 48):
@@ -232,28 +239,29 @@ def main():
 
 	shutil.copy(
 		join(sourceDir, "pixmaps", "starcal.png"),
-		join(shareDir, "pixmaps", iconName)
+		join(shareDir, "pixmaps", iconName),
 	)
 
 	if installType == "for-pkg":
 		runDirStr = join(prefix, "share", pkgName)
 	elif installType == "portable":
-		runDirStr = f'"`dirname \\"\$0\\"`"/../share/{pkgName}'
+		# FIXME: why?
+		runDirStr = join(dirname(dirname(myPath)), "share", pkgName)
 	else:
 		runDirStr = targetCodeDir
 
 	# print(f"runDirStr: {runDirStr}")
 
 	executablePath = join(targetPrefix, "bin", pkgName)
-	with open(executablePath, mode="wt") as _file:
+	with open(executablePath, mode="w", encoding="utf-8") as _file:
 		_file.write(
 			"#!/usr/bin/env sh\n"
-			f'{pyCmd} {runDirStr}/scal3/ui_gtk/starcal-main.py "$@"\n'
+			f'{pyCmd} {runDirStr}/scal3/ui_gtk/starcal-main.py "$@"\n',
 		)
 	os.chmod(executablePath, 0o755)
 	print(f"Executable: {executablePath}")
 
-	with open(join(targetCodeDir, "VERSION"), mode="w") as _file:
+	with open(join(targetCodeDir, "VERSION"), mode="w", encoding="utf-8") as _file:
 		_file.write(f"{version}\n")
 
 	if installType == "system":
@@ -264,7 +272,15 @@ def main():
 	else:
 		os.remove(join(targetCodeDir, "distro/base/uninstall"))
 
-	with open(join(shareDir, "applications", f"{pkgName}.desktop"), mode="w") as _file:
+	with open(
+		join(
+			shareDir,
+			"applications",
+			f"{pkgName}.desktop",
+		),
+		mode="w",
+		encoding="utf-8",
+	) as _file:
 		_file.write(
 			"[Desktop Entry]\n"
 			"Encoding=UTF-8\n"
@@ -277,16 +293,23 @@ def main():
 			"Type=Application\n"
 			"Terminal=false\n"
 			"Categories=GTK;Office;Calendar;Utility;\n"
-			"StartupNotify=true\n"
+			"StartupNotify=true\n",
 		)
 
-	if not subprocess.call([
-		join(sourceDir, "locale.d/install"),
-		targetPrefix,
-	]):
+	res = subprocess.call(
+		[
+			join(sourceDir, "locale.d/install"),
+			targetPrefix,
+		],
+		stdout=sys.stdout,
+		stderr=sys.stderr,
+	)
+	print(f"Script locale.d/install exited with status {res}")
+	if res != 0:
 		return 1
 
-	if "$installType" in ("for-pkg", "system"):
+	if installType in {"for-pkg", "system"}:
+		print("cleaning")
 		cleanup(join(targetCodeDir, ".git"))
 		cleanup(join(targetCodeDir, ".github"))
 		cleanup(join(targetCodeDir, ".gitignore"))
@@ -294,15 +317,19 @@ def main():
 		cleanup(join(targetCodeDir, "google-api-python-client/.git"))
 		cleanup(join(targetCodeDir, "google-api-python-client/.hg"))
 		cleanup(join(targetCodeDir, "screenshots"))
-		# for EXP in '.hidden' '*~' '*.pyc' '*.pyo' '*.tar.xz' '*.tar.gz' '*.deb' '*.rpm' '*.spec':
+		return None
+		# for EXP in '.hidden' '*~' '*.pyc' '*.pyo' '*.tar.xz' '*.tar.gz' '*.deb' \
+		# 	'*.rpm' '*.spec':
 		# 		find "$DIR" -name "$EXP" -exec rm '{}' \; || true
-		# find "$targetCodeDir" -name '__pycache__' -exec rm -R '{}' \; 2>/dev/null || true
+		# find "$targetCodeDir" -name '__pycache__' -exec rm -R '{}' \
+		#  \; 2>/dev/null || true
 		# find "$targetCodeDir" -type d -empty -delete || true
 
-	else:
-		targetDotGit = join(targetCodeDir, ".git")
-		if isdir(targetDotGit):
-			print(f"You may want to remove '{targetDotGit}'")
+	targetDotGit = join(targetCodeDir, ".git")
+	if isdir(targetDotGit):
+		print(f"You may want to remove '{targetDotGit}'")
+		return None
+	return None
 
 
 if __name__ == "__main__":
