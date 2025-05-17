57a8e509f44b31e97dd97e34117d04cda7f97c45
Saeed Rasooli <saeed.gnu@gmail.com>
2025-05-17 05:53:52 +0330

    remove ReadOnlyLabel commented class

--- scal3/ui_gtk/mywidgets/text_widgets.py
+++ scal3/ui_gtk/mywidgets/text_widgets.py
@@ -23,56 +23,6 @@ class ReadOnlyTextWidget:
 	# 	return False
 
 
-# class ReadOnlyLabel(gtk.Label, ReadOnlyTextWidget):
-# 	def get_cursor_position(self):
-# 		return self.get_property("cursor-position")
-
-# 	def get_selection_bound(self):
-# 		return self.get_property("selection-bound")
-
-# 	def has_selection(self):
-# 		return self.get_cursor_position() != self.get_selection_bound()
-
-# 	def copy(self, _item) -> None:
-# 		bound = self.get_selection_bound()
-# 		cursor = self.get_cursor_position()
-# 		start = min(bound, cursor)
-# 		end = max(bound, cursor)
-# 		setClipboard(toStr(self.get_text())[start:end])
-
-# 	def onPopup(self, _widget, menu) -> None:
-# 		# instead of creating a new menu, we should remove all the
-# 		# current items from current menu
-# 		for item in menu.get_children():
-# 			menu.remove(item)
-# 		# ----
-# 		menu.add(
-# 			ImageMenuItem(
-# 				_("Copy _All"),
-# 				imageName="edit-copy.svg",
-# 				func=self.copyAll,
-# 			),
-# 		)
-# 		# ----
-# 		itemCopy = ImageMenuItem(
-# 			_("_Copy"),
-# 			imageName="edit-copy.svg",
-# 			func=self.copy,
-# 		)
-# 		if not self.has_selection():
-# 			itemCopy.set_sensitive(False)
-# 		menu.add(itemCopy)
-# 		# ----
-# 		menu.show_all()
-# 		self.tmpMenu = menu
-# 		ui.updateFocusTime()
-
-# 	def __init__(self, **kwargs) -> None:
-# 		gtk.Label.__init__(self, **kwargs)
-# 		self.set_selectable(True)  # to be selectable, with visible cursor
-# 		self.connect("populate-popup", self.onPopup)
-
-
 class ReadOnlyTextView(gtk.TextView, ReadOnlyTextWidget):
 	def get_text(self) -> str:
 		return buffer_get_text(self.get_buffer())
