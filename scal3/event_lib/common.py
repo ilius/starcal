be04bb83bcae012bb74955758c82698612ec7190
Saeed Rasooli <saeed.gnu@gmail.com>
2025-06-15 18:03:15 +0330

    move getCompactTime and compressLongInt to event_lib

--- scal3/event_lib/common.py
+++ scal3/event_lib/common.py
@@ -3,9 +3,11 @@ from time import time as now
 
 from scal3 import locale_man
 
-__all__ = ["eventTextSep", "getCurrentJd"]
+__all__ = ["compressLongInt", "eventTextSep", "getCompactTime", "getCurrentJd"]
 
-eventTextSep = ": "  # to separate summary from description for display
+
+# to separate summary from description for display
+eventTextSep = ": "
 
 
 def getCurrentJd() -> int:
@@ -20,4 +22,26 @@ def getCurrentJd() -> int:
 			/ 86400
 		)
 		+ 2440588
+	)
+
+
+def compressLongInt(num: int) -> str:
+	"""Num must be less than 2**64."""
+	from base64 import b64encode
+	from struct import pack
+
+	return (
+		b64encode(
+			pack("L", num % 2**64).rstrip(b"\x00"),
+		)[:-3]
+		.decode("ascii")
+		.replace("/", "_")
+	)
+
+
+def getCompactTime(maxDays: int = 1000, minSec: float = 0.1) -> str:
+	return compressLongInt(
+		int(
+			now() % (maxDays * 86400) / minSec,
+		),
 	)
