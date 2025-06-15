530387ec2cb377fc269620d520a14111959dde5e
Saeed Rasooli <saeed.gnu@gmail.com>
2025-06-15 18:00:05 +0330

    copy getCurrentJd func to event_lib

--- scal3/event_lib/common.py
+++ scal3/event_lib/common.py
@@ -1,3 +1,23 @@
+from datetime import datetime
+from time import time as now
+
+from scal3 import locale_man
+
+__all__ = ["eventTextSep", "getCurrentJd"]
+
 eventTextSep = ": "  # to separate summary from description for display
 
-__all__ = ["eventTextSep"]
+
+def getCurrentJd() -> int:
+	tz = locale_man.localTz
+	assert tz is not None
+	epoch = now()
+	return (
+		int(
+			(
+				epoch + int(tz.utcoffset(datetime.fromtimestamp(epoch)).total_seconds())  # noqa: DTZ006
+			)
+			/ 86400
+		)
+		+ 2440588
+	)
