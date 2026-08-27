57161509db00d5a9075bf781e747967ed62725a7
Saeed Rasooli <saeed.gnu@gmail.com>
2026-08-28 02:10:23 +0800

    bugfix in EventGroup: use self.isReadOnly() to respect global allReadOnly

--- scal3/event_lib.py
+++ scal3/event_lib.py
@@ -4184,7 +4184,7 @@ class EventGroup(EventContainer):
 		return allReadOnly or self.__readOnly
 
 	def save(self) -> None:
-		if self.__readOnly:
+		if self.isReadOnly():
 			raise RuntimeError("event group is read-only right now")
 		if self.id is None:
 			self.setId()
